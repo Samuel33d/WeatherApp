eded1649ad510b8588bb8947a956b15696ac657b
Samuel33d <sbenedetti89@gmail.com>
2023-09-30 18:22:25 -0400

    finish project

--- src/components/WeatherStats.jsx
+++ src/components/WeatherStats.jsx
@@ -2,7 +2,7 @@
 const WeatherStats = ({ icon, value, unit }) => {
   return (
     <div className="flex gap-1 justify-center items-center px-2 sm:py-2">
-      <span className="text-2xl sm:text-3xl">{icon}</span>
+      <span className="text-[19.14px] sm:text-3xl">{icon}</span>
       <span>
         {value}
         {unit}
